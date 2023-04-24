552c3916f848663103f78b2d85a2ea7657abb558
ysyx-22041165-cuiyang <shanyangs@outlook.com>
2023-04-24 11:33:03 +0800

    self_test_structure_finish

--- self_test/self_test.v
+++ self_test/self_test.v
@@ -3,48 +3,57 @@ module self_test
 	input wire clk,
 	input wire rst_n,
 	input wire f_layer,
-	input wire upper_story
 	input wire[31:0] data_in,
 
 	output reg en,
 	output reg receive,
-	output reg[31:0] data_out,
-	output wire sort_finish
+	output wire sort_finish,
+	output reg[31:0] data_out
 );
 
 	parameter idle=0, rx_0=1, tx_0=2, rx_1=3, standby=4;
 	reg[2:0] state, next_state;
 	reg[5:0] cnt;
-	reg[3:0] power_set;
+	
+	reg[3:0] p_state, power_value;
 	reg[4:0] chip_id; 
+
 
 /* main_fsm */
 	always@(*)
 		begin
 			case(state)
 				idle: next_state = f_layer?tx_0:rx_0;
-				rx_0:
+				rx_0: //receive_stage
 					begin
-						if(data[15:0] = 16'hBEAF)
+						if(data_in[15:0] == 16'hBEAF)
 							begin
-								power_set = data[29:16];
-								chip_id = data[20:16];
+								chip_id = data_in[20:16];
 								next_state = tx_0;
 							end
 						else
 							next_state = rx_0;
 					end
-				tx_0:
+				tx_0: //transfer_stage
 					begin
-						power;
-						data_out = {2'b10, p_state, chip_id, (chip_id + 1'b1), 16'hBEAF};
+						data_out <= {2'b11, p_state, chip_id, (chip_id + 1'b1), 16'hBEAF};
+						power_value <= p_state; //reg
+						begin
+							if(p_state < 4'b1111)
+								p_state <= p_state + 1;
+							else
+								p_state <= p_state;
+						end
+						next_state <= rx_1;
 					end
 				rx_1: 
 					begin
-						if(cnt == 6'd32 && (data_in <= 32'd3))
+						if((cnt <= 6'd35 && data_in[15:0] == 16'hBEAF) || (cnt >= 6'd35 && p_state == 4'b1111))
+							begin
+								next_state <= standby;
+							end
+						else
 							next_state <= tx_0;
-						else if(data[15:0] == 16'hBEAF || p_state == 4'b1111)
-							next_state <= standby;
 					end
 				standby:
 					next_state <= standby;
@@ -61,195 +70,24 @@ module self_test
 					en <= 0;
 					chip_id <= 0;
 					cnt <= 0;
+					p_state <= 4'b0001;
+				end
+			else if(state == rx_1)
+				begin
+					if(cnt == 6'd35)
+						cnt <= 'b0;
+					else
+						begin
+							cnt <= cnt + 1'b1;
+							state <= next_state;
+						end
 				end
 			else
-				cnt <= cnt + 1'b1;
-				state <= next_state;
+				begin
+					state <= next_state;
+				end
 		end
 
 assign sort_finish = (state == standby);
-
-/* sub_fsm */
-reg[3:0] p_cnt;
-reg[3:0] p_state,p_next_state;
-
-task power;
-	begin
-		case(p_state)
-			4'b0001:
-				begin
-					if(p_cnt == 0)
-						begin
-							p_next_state <= p_state;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b0010:
-				begin
-					if(p_cnt == 1)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b0011:
-				begin
-					if(p_cnt == 2)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state_next <= p_state + 1'b1;
-				end
-
-			4'b0100:
-				begin
-					if(p_cnt == 3)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b0101:
-				begin
-					if(p_cnt == 4)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b0110:
-				begin
-					if(p_cnt == 5)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b0111:
-				begin
-					if(p_cnt == 6)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1000:
-				begin
-					if(p_cnt == 7)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1001:
-				begin
-					if(p_cnt == 8)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1010:
-				begin
-					if(p_cnt == 9)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1011:
-				begin
-					if(p_cnt == 10)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1100:
-				begin
-					if(p_cnt == 11)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1101:
-				begin
-					if(p_cnt == 12)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1110:
-				begin
-					if(p_cnt == 13)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-			4'b1111:
-				begin
-					if(p_cnt == 14)
-						begin
-							p_next_state <= p_state;
-							p_cnt <= p_cnt + 1'b1;
-						end
-					else	
-						p_next_state <= p_state + 1'b1;
-				end
-
-		endcase
-	end
-endtask
-
-	always@(posedge clk or negedge rst_n)
-		begin
-			if(!rst_n)
-				p_state <= 4'b0001;
-			else
-				p_state <= p_next_state;
-		end
 
 endmodule
