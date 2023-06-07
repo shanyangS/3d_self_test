ee9374d349044280d5bd2f1d02cf8d7c828ab9df
ysyx-22041165-cuiyang <shanyangs@outlook.com>
2023-06-07 21:18:55 +0800

    synthesis_v2.1

--- self_test/self_test.v
+++ self_test/self_test.v
@@ -1,91 +1,115 @@
-module top (
-    input wire t_clk,
-    input wire rst_n,
+module self_test
+(
+	input wire div_8_clk,
+	input wire rst_n,
+	input wire f_layer,
+	input wire[31:0] data_in,
 
-    input wire f_layer,
-
-    input wire data_in,
-
-    output wire sort_finish,
-    output wire data_out   
+	output wire tx_out,
+	output wire sort_finish,
+	output reg[31:0] data_out
 );
 
-/* general */
-wire div_8_clk;
-wire rst_sync_o;
+	parameter idle=0, rx_0=1, tx_0=2, rx_1=3, standby=4;
+	reg[2:0] state, next_state;
+	reg[4:0] cnt;
+	
+	reg[3:0] power_value;
+	reg[3:0] chip_id;
 
-/* deserializer */
-wire[7:0] des_data_out;
 
-/* eight_to_thirty_two */
-wire[31:0] ettt_data_out;
+/* main_fsm */
+always@(*) begin
+	case(state)
+		idle: begin
+			if(f_layer)
+				next_state = tx_0;
+			else
+				next_state = rx_0;
+		end
+		rx_0: begin //receive_stage 
+			if(data_in[15:0] == 16'hBEEF)
+				next_state = tx_0;
+			else
+				next_state = rx_0;
+		end
+		tx_0: begin //transfer_stage
+			next_state = rx_1;
+		end
+		rx_1: begin
+			if((cnt <= 5'd20 && data_in[15:0] == 16'hBEEF && data_in[23:20] == (chip_id + 1'b1)) || (cnt >= 5'd20 && power_value == 4'b1111))
+				next_state = standby;
+			else if(cnt >= 5'd20)
+				next_state = tx_0;
+			else
+				next_state = rx_1;
+		end
+		standby: begin
+			next_state = standby;
+		end
+		default: next_state = state;
+	endcase
+end
 
-/* self_test */
-wire tx_out;
-wire[31:0] st_data_out; 
+/* state */
+always@(posedge div_8_clk or negedge rst_n) begin
+	if(!rst_n) begin
+		state <= idle;
+		cnt <= 0;
+	end else if(state != rx_1) begin
+		cnt <= 'b0;
+		state <= next_state;
+	end else if(state == rx_1) begin
+		if(cnt == 5'd21)
+			cnt <= 'b0;
+		else begin
+			cnt <= cnt + 1'b1;
+			state <= next_state;
+		end
+	end else
+		state <= next_state;
+end
 
-/* thirty_two_to_eight */
-wire[7:0] ttte_data_out;
+/* power_value */
+always@(posedge div_8_clk or negedge rst_n) begin
+    if(!rst_n)
+		power_value <= 4'b0000;
+    else if(next_state == tx_0 && power_value < 4'b1111)
+        power_value <= power_value + 1;
+	else
+		power_value <= power_value;
+end
 
-clk_eight_div clk_eight_div (
-.t_clk(t_clk),
-.rst_n(rst_n),
+/* chip_id */
+always@(posedge div_8_clk or negedge rst_n) begin
+	if(!rst_n)
+		chip_id <= 4'b0000;
+	else case(state)
+		idle: begin
+			if(f_layer)
+				chip_id <= 4'b0001;
+			else
+				chip_id <= 4'b0000;
+		end
+		rx_0: begin
+			if(data_in[15:0] == 16'hBEEF)
+				chip_id <= data_in[19:16];
+			else
+				chip_id <= chip_id;
+		end
+		default: chip_id <= chip_id;	
+	endcase
+end
 
-.div_8_clk(div_8_clk)
-);
+/* data_out */
+always@(*) begin
+	case(state)
+		tx_0: data_out = {4'b1010, power_value, chip_id, (chip_id + 1'b1), 16'hBEEF};
+		default: data_out = 'b0;
+	endcase
+end
 
-sync_async_reset sync_async_reset (
-.div_8_clk(div_8_clk),
-.rst_n(rst_n),
+assign tx_out = (state == tx_0);
+assign sort_finish = (state == standby) || f_layer;
 
-.rst_sync_o(rst_sync_o)
-);
-
-deserializer deserializer (
-.t_clk(t_clk),
-.rst_n(rst_sync_o),
-
-.data_in(data_in),
-
-.data_out(des_data_out)
-);
-
-eight_to_thirty_two eight_to_thirty_two (
-.div_8_clk(div_8_clk),
-.rst_n(rst_sync_o),
-.data_in(des_data_out),
-
-.data_out(ettt_data_out)
-);
-
-self_test self_test (
-.div_8_clk(div_8_clk),
-.rst_n(rst_sync_o),
-.f_layer(f_layer),
-.data_in(ettt_data_out),
-
-.tx_out(tx_out),
-.sort_finish(sort_finish),
-.data_out(st_data_out)
-);
-
-thirty_two_to_eight thirty_two_to_eight (
-.div_8_clk(div_8_clk),
-.rst_n(rst_sync_o),
-.tx_out(tx_out),
-
-.data_in(st_data_out),
-
-.data_out(ttte_data_out)
-);
-
-serializer serializer (
-.t_clk(t_clk),
-.rst_n(rst_sync_o),
-
-.data_in(ttte_data_out),
-
-.data_out(data_out)
-);
-    
 endmodule
